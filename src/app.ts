e582554a7517c1c905b8af3469c4f8994a9fdf50
Javid Ahmad <dev.javid@outlook.com>
2023-02-01 19:29:10 +0530

    old r1

--- src/app.ts
+++ src/app.ts
@@ -1,72 +1,38 @@
-import compression from 'compression';
-import cors from 'cors';
 import express from 'express';
-import helmet from 'helmet';
-import hpp from 'hpp';
-import { connect, set } from 'mongoose';
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import cors from "cors";
+import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
 import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from './config';
-import { dbConnection } from './databases';
-import { Routes } from './interfaces/routes.interface';
-import errorMiddleware from './middlewares/error.middleware';
-import { itemsRouter } from 'items/items.router';
+import hpp from 'hpp';
+import compression from 'compression';
 
 class App {
   public app: express.Application;
   public env: string;
   public port: string | number;
-
   constructor() {
     this.port = parseInt(process.env.PORT as string, 10);
     this.app = express();
  
-    // this.connectToDatabase();
+    //this.connectToDatabase();
     this.initializeMiddlewares();
     //this.initializeRoutes(routes);
     // this.initializeSwagger();
     // this.initializeErrorHandling();
   }
-
   public listen() {
     this.app.listen(this.port, () => {
       console.log(`Listening on port ${this.port}`);
     });
   }
-
-  private connectToDatabase() {
-    if (this.env !== 'production') {
-      set('debug', true);
-    }
-
-    connect(dbConnection.url);
-  }
-
-  private initializeSwagger() {
-    const options = {
-      swaggerDefinition: {
-        info: {
-          title: 'REST API',
-          version: '1.0.0',
-          description: 'Example docs',
-        },
-      },
-      apis: ['swagger.yaml'],
-    };
-
-    const specs = swaggerJSDoc(options);
-    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-  }
-
   private initializeMiddlewares() {
     this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
     this.app.use(hpp());
     this.app.use(helmet());
     this.app.use(cors());
-    // this.app.use(compression());
+    this.app.use(compression());
     this.app.use(express.json());
     this.app.use("/api/menu/items", itemsRouter);
   }
 }
-
 export default App;
