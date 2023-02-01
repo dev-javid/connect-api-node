48203e5e0301409e530a439e19016e02043beeee
Javid Ahmad <dev.javid@outlook.com>
2023-02-01 19:22:55 +0530

    r4

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,7 @@ class App {
     // this.connectToDatabase();
     this.initializeMiddlewares();
     //this.initializeRoutes(routes);
-    this.initializeSwagger();
+    // this.initializeSwagger();
     // this.initializeErrorHandling();
   }
 
