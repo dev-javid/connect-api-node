8145af8124198777a7036a362e1cde1d79483347
Javid Ahmad <dev.javid@outlook.com>
2023-02-02 13:43:06 +0530

    release-03022023-3

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -10,7 +10,7 @@ const authMiddleware =
   (...areas: string[]) =>
   async (req: RequestWithUser, res: Response, next: NextFunction) => {
     try {
-      const Authorization = req.cookies['Authorization'] || (req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null);
+      const Authorization = (req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null);
 
       if (Authorization) {
         const secretKey: string = SECRET_KEY;
